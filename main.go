fe020417ea16139e5198bab99551293576c9b6a0
tj562y <tj562y@att.com>
2019-04-05 10:00:58 -0700

    Refactor and fill out delete functions

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"k8s.io/api/apps/v1"
+	v1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	typedappsv1 "k8s.io/client-go/kubernetes/typed/apps/v1"
 	"k8s.io/client-go/rest"
 )
 
@@ -14,21 +15,35 @@ func (b bin) String() string {
 	return fmt.Sprintf("%b", b)
 }
 
-type DeleteFunc func(deployment *v1.Deployment, restarts int) (bool, error)
+type DeleteFunc func(deploymentInterface typedappsv1.DeploymentInterface, deployment *v1.Deployment, restarts int, restartThreshold int) (bool, error)
 
-func DeleteCrash(deployment *v1.Deployment, restarts int) (bool, error) {
+func DeleteCrash(deploymentInterface typedappsv1.DeploymentInterface, deployment *v1.Deployment, restarts int, restartThreshold int) (bool, error) {
 	// do your crash loop logic in here
-
-	return true, nil
+	if restartThreshold > 0 && restarts >= restartThreshold {
+		return DeleteGeneric(deploymentInterface, deployment, restarts, restartThreshold)
+	} else {
+		fmt.Printf("%s/%s is in CrashLoop state, but not deleting. Restarts/Threshold = %s/%s", deployment.Namespace, deployment.Name, restarts, restartThreshold)
+		return false, nil
+	}
 }
 
-func DeleteGeneric(deployment *v1.Deployment, restarts int) (bool, error) {
+func DeleteGeneric(deploymentInterface typedappsv1.DeploymentInterface, deployment *v1.Deployment, restarts int, restartThreshold int) (bool, error) {
 	// do your generic logic in here
+	policy := metav1.DeletePropagationForeground
+	gracePeriodSeconds := int64(0)
+	fmt.Printf("About to delete %s/%s and its associated resources.\n", deployment.Namespace, deployment.Name)
+
+	err := deploymentInterface.Delete(deployment.Name, &metav1.DeleteOptions{PropagationPolicy: &policy, GracePeriodSeconds: &gracePeriodSeconds})
+	if err != nil {
+		fmt.Printf("%s/%s, Error: %s \n", deployment.Namespace, deployment.Name, err.Error())
+		return false, err
+	}
 
 	return true, nil
 }
 
 func main() {
+
 	// initialize the config, from yaml or environment variables
 	var kleanerConfig = ConfigObj
 	// create the map that will hold the reasons and the config object
@@ -39,10 +54,12 @@ func main() {
 		waitingReasons[conf.Reason] = conf
 	}
 
+	// fail fast if not in the cluster
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
 	}
+
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
@@ -56,9 +73,9 @@ func main() {
 			panic(err.Error())
 		}
 
-		for _, item := range pods.Items {
+		for _, pod := range pods.Items {
 		StatusLoop:
-			for _, status := range item.Status.ContainerStatuses {
+			for _, status := range pod.Status.ContainerStatuses {
 				waiting := status.State.Waiting
 				if waiting != nil {
 					reason := waiting.Reason
@@ -66,70 +83,34 @@ func main() {
 					crawlerConfigDetails, ok := waitingReasons[reason]
 					if ok {
 
-						rs, err := clientset.AppsV1().ReplicaSets(item.Namespace).Get(item.OwnerReferences[0].Name, metav1.GetOptions{})
+						rs, err := clientset.AppsV1().ReplicaSets(pod.Namespace).Get(pod.OwnerReferences[0].Name, metav1.GetOptions{})
 						if err != nil {
 							fmt.Printf("Error retrieving ReplicaSets. Error: %s\n", err.Error())
 							continue StatusLoop
 						}
 
 						if rs.OwnerReferences != nil {
-							deploy, err := clientset.AppsV1().Deployments(item.Namespace).Get(rs.OwnerReferences[0].Name, metav1.GetOptions{})
-
+							deploy, err := clientset.AppsV1().Deployments(pod.Namespace).Get(rs.OwnerReferences[0].Name, metav1.GetOptions{})
 							if err != nil {
-								// do some more error handling
+								fmt.Printf("Error retrieving Deployments. Error: %s\n", err.Error())
+								continue StatusLoop
 							}
+							if deploy != nil && deploy.Name != "" { // indicates something to be deleted
+								// this is where the new interface method(s) come in.
+								// The restart threshold will be 0 if not specified in the config, so have to handle that case.
+								_, err = crawlerConfigDetails.DeleteFunction(clientset.AppsV1().Deployments(pod.Namespace), deploy, int(status.RestartCount), crawlerConfigDetails.RestartThreshold)
 
-							// this is where the new interface method(s) come in.
-							// The restart threshold will be 0 if not specified in the config, so have to handle that case.
-							_, err = crawlerConfigDetails.DeleteFunction(deploy, crawlerConfigDetails.RestartThreshold)
+								if err != nil {
+									// do even more error handling if you don't change the return signature
+								}
 
-							if err != nil {
-								// do even more error handling if you don't change the return signature
+							} else {
+								fmt.Println("No deployment found.")
 							}
+						} else {
+							fmt.Println("No replica set owner reference.")
 						}
 					}
-
-					// Tremaine - you still have to untangle all of this and put the right logic into the right Delete function
-					//_, reasonInWaitingReasons := waitingReasons[reason]
-					//if reasonInWaitingReasons || (reason == "CrashLoopBackOff" && status.RestartCount > restartThreshold) {
-					//	if reason == "CrashLoopBackOff" {
-					//		fmt.Printf("%s / %s has %s restarts, which is over the %s restart limit.", item.Namespace,
-					//			item.Name, strconv.Itoa(int(status.RestartCount)), strconv.Itoa(int(restartThreshold)))
-					//	} else if reasonInWaitingReasons {
-					//		fmt.Printf("%s / %s has a status of %s, which is configured to be deleted.", item.Namespace,
-					//			item.Name, reason)
-					//	}
-					//
-					//	rs, err := clientset.AppsV1().ReplicaSets(item.Namespace).Get(item.OwnerReferences[0].Name, metav1.GetOptions{})
-					//	if err != nil {
-					//		fmt.Printf("Error retrieving ReplicaSets. Error: %s\n", err.Error())
-					//		continue StatusLoop
-					//	}
-					//
-					//	if rs.OwnerReferences != nil {
-					//		deploy, err := clientset.AppsV1().Deployments(item.Namespace).Get(rs.OwnerReferences[0].Name, metav1.GetOptions{})
-					//		if err != nil {
-					//			fmt.Printf("Error retrieving Deployments. Error: %s\n", err.Error())
-					//			continue StatusLoop
-					//		}
-					//		if deploy != nil {
-					//			if deploy.Name != "" {
-					//				policy := metav1.DeletePropagationForeground
-					//				gracePeriodSeconds := int64(0)
-					//				fmt.Printf("About to delete %s/%s and its associated resources.\n", item.Namespace, deploy.Name)
-					//				err := clientset.AppsV1().Deployments(item.Namespace).Delete(rs.OwnerReferences[0].Name, &metav1.DeleteOptions{PropagationPolicy: &policy, GracePeriodSeconds: &gracePeriodSeconds})
-					//				if err != nil {
-					//					fmt.Printf("%s/%s, Error: %s \n", item.Namespace, deploy.Name, err.Error())
-					//					continue StatusLoop
-					//				}
-					//			} else {
-					//				fmt.Println("No deployment name.")
-					//			}
-					//		}
-					//	} else {
-					//		fmt.Println("No replica set owner reference.")
-					//	}
-					//}
 				}
 			}
 		}
