df4b07ed54ffceda8a03ce8a1705bf3470d033cb
Tremaine Eto <te1575@att.com>
2019-04-08 14:43:51 -0700

    Add comments
    - Add logging statements
    - Consolidate one if check

--- main.go
+++ main.go
@@ -18,17 +18,20 @@ func (b bin) String() string {
 type DeleteFunc func(deploymentInterface typedappsv1.DeploymentInterface, deployment *v1.Deployment, restarts int, restartThreshold int) (bool, error)
 
 func DeleteCrash(deploymentInterface typedappsv1.DeploymentInterface, deployment *v1.Deployment, restarts int, restartThreshold int) (bool, error) {
-	// do your crash loop logic in here
+	// pod is in a CrashLoopBackOff state
+	// check if restart number meets or exceeds the restart threshold
+	// the restart threshold will be 0 if not specified in the config, so have to handle that case
 	if restartThreshold > 0 && restarts >= restartThreshold {
 		return DeleteGeneric(deploymentInterface, deployment, restarts, restartThreshold)
 	} else {
-		fmt.Printf("%s/%s is in CrashLoop state, but not deleting. Restarts/Threshold = %s/%s", deployment.Namespace, deployment.Name, restarts, restartThreshold)
+		fmt.Printf("%s/%s is in a CrashLoopBackOff state, but doesn't meet the restart threshold. " +
+			"Restarts/Threshold = %v/%v", deployment.Namespace, deployment.Name, restarts, restartThreshold)
 		return false, nil
 	}
 }
 
 func DeleteGeneric(deploymentInterface typedappsv1.DeploymentInterface, deployment *v1.Deployment, restarts int, restartThreshold int) (bool, error) {
-	// do your generic logic in here
+	// pod is in a state defined in config.yaml
 	policy := metav1.DeletePropagationForeground
 	gracePeriodSeconds := int64(0)
 	fmt.Printf("About to delete %s/%s and its associated resources.\n", deployment.Namespace, deployment.Name)
@@ -38,12 +41,12 @@ func DeleteGeneric(deploymentInterface typedappsv1.DeploymentInterface, deployme
 		fmt.Printf("%s/%s, Error: %s \n", deployment.Namespace, deployment.Name, err.Error())
 		return false, err
 	}
+	fmt.Printf("Deleted %s/%s and its associated resources.\n", deployment.Namespace, deployment.Name)
 
 	return true, nil
 }
 
 func main() {
-
 	// initialize the config, from yaml or environment variables
 	var kleanerConfig = ConfigObj
 	// create the map that will hold the reasons and the config object
@@ -68,10 +71,12 @@ func main() {
 	fmt.Println("Beginning the crawl.")
 
 	for {
+		// get a list of pods for all namespaces
 		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
 		if err != nil {
 			panic(err.Error())
 		}
+		fmt.Println("Got list of pods.")
 
 		for _, pod := range pods.Items {
 		StatusLoop:
@@ -79,16 +84,14 @@ func main() {
 				waiting := status.State.Waiting
 				if waiting != nil {
 					reason := waiting.Reason
-
-					crawlerConfigDetails, ok := waitingReasons[reason]
-					if ok {
-
+					if crawlerConfigDetails, ok := waitingReasons[reason]; ok {
+						fmt.Printf("Waiting reason match: %s/%s has a waiting reason of: %s", pod.Namespace,
+							pod.OwnerReferences[0].Name, reason)
 						rs, err := clientset.AppsV1().ReplicaSets(pod.Namespace).Get(pod.OwnerReferences[0].Name, metav1.GetOptions{})
 						if err != nil {
 							fmt.Printf("Error retrieving ReplicaSets. Error: %s\n", err.Error())
 							continue StatusLoop
 						}
-
 						if rs.OwnerReferences != nil {
 							deploy, err := clientset.AppsV1().Deployments(pod.Namespace).Get(rs.OwnerReferences[0].Name, metav1.GetOptions{})
 							if err != nil {
@@ -96,14 +99,11 @@ func main() {
 								continue StatusLoop
 							}
 							if deploy != nil && deploy.Name != "" { // indicates something to be deleted
-								// this is where the new interface method(s) come in.
-								// The restart threshold will be 0 if not specified in the config, so have to handle that case.
 								_, err = crawlerConfigDetails.DeleteFunction(clientset.AppsV1().Deployments(pod.Namespace), deploy, int(status.RestartCount), crawlerConfigDetails.RestartThreshold)
-
 								if err != nil {
-									// do even more error handling if you don't change the return signature
+									fmt.Printf("Error deleting Deployment. Error: %s\n", err.Error())
+									continue StatusLoop
 								}
-
 							} else {
 								fmt.Println("No deployment found.")
 							}
